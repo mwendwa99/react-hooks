2ab2462d89bad3c375787ea8dc66202367db8742
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2021-09-06 09:31:41 +0300

    refactor Counter to use useReducer hook

--- src/Components/Hooks.jsx
+++ src/Components/Hooks.jsx
@@ -1,55 +1,47 @@
-import React, { useState } from 'react';
-// import ''
+import React, { useState, useReducer } from 'react';
 
+const initialState = { count: 0 };
+
+const reducerFunc = (state, action) => {
+    switch (action.type) {
+        case 'increase':
+            return { count: state.count + 1 };
+        case 'decrease':
+            return { count: state.count - 1 };
+        default:
+            return { state }
+    }
+}
 
 export const Hooks = ({ max }) => {
 
-    const [count, setCount] = useState(0);
+    const [state, dispatch] = useReducer(reducerFunc, initialState);
 
     const [num, setNum] = useState(0)
     const randomNum = () => setNum(Math.floor(Math.random() * (9999 - 1000 + 1) + 1000));
 
-    const countUp = () => setCount(count + 1);
-    const countDown = () => setCount(count - 1);
-
-    const upperLimit = max
-
-    if (upperLimit - count === -1) {
-        setCount(upperLimit)
+    const upperLimit = () => {
+        state.count !== max ? dispatch({ type: 'increase' }) : console.log('max!')
     }
-    if (count === -1) {
-        setCount(0)
+    const lowerLimit = () => {
+        state.count !== 0 ? dispatch({ type: 'decrease' }) : console.log('min')
     }
 
     return (
         <div className='hook'>
-            {/* <div> */}
             <div>
-                <h1> Counter: {count} </h1>
+                <h1> Counter: {
+                    state.count === max ? 'limits reached!' :
+                        state.count === 0 ? 'limits reached!' : state.count
+                } </h1>
                 <h1>passcode <br /> generator: {num} </h1>
             </div>
             <div className="btn">
-                <button onClick={countUp} >Count Up</button>
-                <button onClick={countDown} >Count Down</button>
+                <button onClick={upperLimit} >Count Up</button>
+                <button onClick={lowerLimit} >Count Down</button>
                 <button onClick={randomNum} >generate</button>
             </div>
-            {/* </div> */}
 
         </div>
     );
 }
-
-// export const PasscodeGenerator = () => {
-
-
-
-
-//     return (
-//         <div className='hook' >
-
-//             <div>
-
-//             </div>
-//         </div>
-//     )
-// }
